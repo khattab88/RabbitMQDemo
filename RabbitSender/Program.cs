4725d9a34c98edd10c1bbb3a88aa372bc3184d58
khattab88 <m_khatab_88@hotmail.com>
2023-11-10 21:32:29 +0200

    Add project files.

---
+++ RabbitSender/Program.cs
@@ -0,0 +1,23 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+ConnectionFactory connectionFactory = new();
+connectionFactory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+connectionFactory.ClientProvidedName = "Rabbit Sender App";
+
+IConnection conn = connectionFactory.CreateConnection();
+IModel channel = conn.CreateModel();
+
+string exchangeName = "DemoExchange";
+string routingKey = "demo-routing-key";
+string queueName = "DemoQueue";
+
+channel.ExchangeDeclare(exchangeName, ExchangeType.Direct);
+channel.QueueDeclare(queueName, durable: false, exclusive: false, autoDelete: false, arguments: null);
+channel.QueueBind(queueName, exchangeName, routingKey, null);
+
+byte[] messageBodyBytes = Encoding.UTF8.GetBytes("Hello RabbitMQ");
+channel.BasicPublish(exchangeName, routingKey, null, messageBodyBytes);
+
+channel.Close();
+conn.Close();
