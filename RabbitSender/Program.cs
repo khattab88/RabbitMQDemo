1fb99acbdca5152cb0b804c839bc0af48f3210ec
khattab88 <m_khatab_88@hotmail.com>
2023-11-10 21:38:38 +0200

    configure and send first message

--- RabbitSender/Program.cs
+++ RabbitSender/Program.cs
@@ -1,6 +1,7 @@
 ﻿using RabbitMQ.Client;
 using System.Text;
 
+// configure connection
 ConnectionFactory connectionFactory = new();
 connectionFactory.Uri = new Uri("amqp://guest:guest@localhost:5672");
 connectionFactory.ClientProvidedName = "Rabbit Sender App";
@@ -8,6 +9,7 @@ connectionFactory.ClientProvidedName = "Rabbit Sender App";
 IConnection conn = connectionFactory.CreateConnection();
 IModel channel = conn.CreateModel();
 
+// configure exchange, queue, binding
 string exchangeName = "DemoExchange";
 string routingKey = "demo-routing-key";
 string queueName = "DemoQueue";
@@ -16,6 +18,7 @@ channel.ExchangeDeclare(exchangeName, ExchangeType.Direct);
 channel.QueueDeclare(queueName, durable: false, exclusive: false, autoDelete: false, arguments: null);
 channel.QueueBind(queueName, exchangeName, routingKey, null);
 
+// send message
 byte[] messageBodyBytes = Encoding.UTF8.GetBytes("Hello RabbitMQ");
 channel.BasicPublish(exchangeName, routingKey, null, messageBodyBytes);
 
